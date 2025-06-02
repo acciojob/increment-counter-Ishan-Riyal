5a38500f40bddc0b631035abe956d8ff0c48ab98
Ishan Riyal <ishanriyal07@gmail.com>
2025-06-02 14:37:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const count = document.getElementById('counter');
+const btnInc = document.getElementById('incrementBtn');
+
+btnInc.addEventListener('click', ()=>{
+	count++
+})
